7af4241106778e68a2a8848e6c692622d05fc82b
modac <juni@aresa.io>
2024-12-07 15:28:17 +0900

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,41 @@
-import { News } from "@/interface/dtos/news";
-import axios from "axios";
+"use client";
+
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { News } from "@/interface/dtos/news";
 
-const fetchPost = async () => {
-  const res = await fetch('/api/weeklyNews', {
-    cache: "no-store",
-  })
-  return res.json();
-}
+const Home = () => {
+  const [data, setData] = useState<News.weeklyNews[]>([]);
 
-const Home = async () => {
-  const data = await fetchPost();
-  console.log(data);
-  return <div></div>;
-}
+  const fetchPost = async () => {
+    try {
+      const res = await axios.get('/api/weeklyNews');
+      const serverData: News.weeklyNews[] = res.data.results;
+      setData((initialData) => [...initialData, ...serverData]);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  useEffect(() => {
+    console.log("State updated:", data);
+  }, [data]);
+
+
+  return <div>
+    {data.map(({ title, id, media, }) =>
+      <ul key={id}>
+        <li>
+          <h3>{title}</h3>
+          <img src={media[0]["media-metadata"][2].url} alt="" />
+        </li>
+      </ul>
+    )}
+  </div>;
+};
 
 export default Home;
-
-
